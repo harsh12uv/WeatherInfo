762f7ad2a4c08c19da4150c43b382b9280bcdbef
harsh12uv <harsharaj2099@gmail.com>
2024-08-03 20:21:45 +0530

    almost done

--- script2.js
+++ script2.js
@@ -63,12 +63,45 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${lon
     .then(data => {
         // Extract the required information
         const location = data.name;
-        const windspeed = data.wind.speed;
+        const windspeed = Math.round(data.wind.speed*18/5 * 100) / 100;
         const humidity = data.main.humidity;
         const timezone = data.timezone;
         const pressure = data.main.pressure;
-        const windDirection = data.wind.deg;
+        const degrees = data.wind.deg;
         const feelsLike = data.main.feels_like;
+
+        let result = timezone/ 3600;
+        let hrs = Math.trunc(result); // Get the integer part
+        let decimalPart = result - hrs;
+        let minutes=decimalPart*60;
+
+        
+        let timeZone=`GMT + ${hrs}:${minutes}`;
+
+        const directions = ['N', 'N-NE', 'NE', 'E-NE', 'E', 'E-SE', 'SE', 'S-SE', 'S', 'S-SW', 'SW', 'W-SW', 'W', 'W-NW', 'NW', 'N-NW', 'N'];
+        const index = Math.round(degrees / 22.5) % 16;
+
+        const windDirection=directions[index];
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
+        
+
+        
+
+
+        
 
         // // Get latitude and longitude for the One Call API
         // const lat = data.coord.lat;
@@ -84,12 +117,13 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${lon
                 const weatherInfoDiv = document.getElementById('weather-info');
                 weatherInfoDiv.innerHTML = `
                     <p>Location: ${location}</p>
-                    <p>Wind Speed: ${windspeed} m/s</p>
+                    <p>Wind Speed: ${windspeed} km/hr</p>
                     <p>Humidity: ${humidity}%</p>
-                    <p>Timezone: ${timezone}</p>
+                    <p>Timezone: ${timeZone}</p>
                     <p>Pressure: ${pressure} hPa</p>
-                    <p>Wind Direction: ${windDirection}°</p>
+                    <p>Wind Direction: ${windDirection}</p>
                     <p>Feels Like: ${feelsLike}°C</p>
+                   
                    
                `;
             // });
